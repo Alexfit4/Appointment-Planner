ab1840e65afa36d30b007b0056f07ed89553e7a3
alexfit4 <alex.ashtiany@gmail.com>
2021-03-28 19:34:31 -0400

    updating

---
+++ src/components/contactForm/ContactForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export const ContactForm = ({
+  name,
+  setName,
+  phone,
+  setPhone,
+  email,
+  setEmail,
+  handleSubmit
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        value={name}
+        onChange={({target}) => {setName(target.value)}}
+        required
+      />
+      <input 
+        type="tel"
+        value={phone}
+        onChange={({target}) => {setPhone(target.value)}}
+        required
+      />
+      <input 
+        type="email"
+        value={email}
+        onChange={({target}) => {setEmail(target.value)}}
+        required
+      />
+      <input
+        type="submit"
+      />
+    </form>
+  );
+};
+
+export default ContactForm;
